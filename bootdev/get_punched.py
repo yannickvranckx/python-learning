c704f75d10b89dd6d273ec15de7f79a0921906ef
Yannick Vranckx <41998248+yannickvranckx@users.noreply.github.com>
2025-01-06 16:52:16 +0100

    Create get_punched.py

---
+++ bootdev/get_punched.py
@@ -0,0 +1,36 @@
+# This creates 2 functions, get punched and get slashed
+# Both functions will take health and armor as PARAMETERS, but armor has a default value of 0
+# damage should be 50 - the armor and new_health should return your health minus the damage you sustained
+def get_punched(health, armor=0):
+    damage = 50 - armor
+    new_health = health - damage
+    return new_health
+
+
+def get_slashed(health, armor=0):
+    damage = 100 - armor
+    new_health = health - damage
+    return new_health
+
+
+# Don't touch below this line
+
+
+def test(health, armor):
+    print(f"Health: {health}, Armor: {armor}")
+    print(f"Health after punch: {get_punched(health, armor)}")
+    print("=====================================")
+    print(f"Health: {health}, Armor: {armor}")
+    print(f"Health after slash: {get_slashed(health, armor)}\n")
+    print("=====================================")
+    print(f"Health: {health}, Armor: no armor!")
+    print(f"Health after slash: {get_slashed(health)}\n")
+    print("=====================================")
+    print(f"Health: {health}, Armor: no armor!")
+    print(f"Health after punch: {get_punched(health)}")
+    print("=====================================")
+
+
+test(400, 5)
+test(300, 3)
+test(200, 1)
